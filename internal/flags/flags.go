1fc8de88f2c8af73ac03de095b049c9123d0c9b2
Rodolfo Campos <camposer@gmail.com>
2022-03-30 11:34:57 +0200

    Update flags.go

--- internal/flags/flags.go
+++ internal/flags/flags.go
@@ -6,10 +6,10 @@ import (
 )
 
 var GoogleFlag = flag.Bool("google", false, "use Google as a Source")
-var DeleteGroupsNotInIdPFlag = flag.Bool("delete-groups-missing-in-idp", false, "delete groups present in SDM but not in selected Source data")
-var DeleteUsersNotInIdPFlag = flag.Bool("delete-users-missing-in-idp", false, "delete users present in SDM but not in the selected Source data")
+var DeleteGroupsNotInIdPFlag = flag.Bool("delete-groups-missing-in-idp", false, "delete groups present in SDM but not in the selected Identity Provider")
+var DeleteUsersNotInIdPFlag = flag.Bool("delete-users-missing-in-idp", false, "delete users present in SDM but not in the selected Identity Provider")
 var PlanFlag = flag.Bool("plan", false, "do not apply changes just show initial queries")
-var QueryFlag = flag.String("query", "", "pass a query according to the available query syntax for the selected IdP")
+var QueryFlag = flag.String("query", "", "pass a query according to the available query syntax for the selected Identity Provider")
 
 func ValidateMandatoryFlags() error {
 	if !*GoogleFlag {
